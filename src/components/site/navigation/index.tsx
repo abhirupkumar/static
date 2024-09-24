cddd54ca5a29adb4ce8687f819964841eea0c69f
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-09-24 12:13:05 +0530

    Refactor navigation component styles and links

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,10 +1,11 @@
 import { ModeToggle } from '@/components/global/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/server'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import localFont from "next/font/local";
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
 
 type Props = {
   user?: null | User
@@ -16,28 +17,55 @@ const novaSquare = localFont({
 
 const Navigation = ({ user }: Props) => {
   return (
-    <div className="absolute top-0 left-0 right-0 z-10 p-4 flex items-center justify-between backdrop-blur-lg">
-      <aside className="flex items-center">
-        <span className={`text-xl font-bold ${novaSquare.className}`}> Biznex</span>
+    <header className="absolute top-0 left-0 right-0 z-10 p-4 flex items-center justify-between">
+      <aside className="flex items-center gap-2">
+        <span className={`text-xl font-bold z-10 ${novaSquare.className}`}>Biznex</span>
       </aside>
-      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
-        <ul className="flex items-center justify-center gap-8">
-          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>Pricing</Link>
-          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>About</Link>
-          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>Features</Link>
+      <nav className="hidden md:block absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <ul className="flex items-center gap-8">
+          <li>
+            <Link
+              className={cn(
+                buttonVariants({ variant: "link" }),
+                "text-inherit p-0 underline-offset-8"
+              )}
+              href="#"
+            >
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={cn(
+                buttonVariants({ variant: "link" }),
+                "text-inherit p-0 underline-offset-8"
+              )}
+              href="#"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={cn(
+                buttonVariants({ variant: "link" }),
+                "text-inherit p-0 underline-offset-8"
+              )}
+              href="#"
+            >
+              Features
+            </Link>
+          </li>
         </ul>
       </nav>
-      <aside className="flex gap-2 items-center">
-        <Link
-          href={'/agency'}
-          className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
-        >
-          Login
+      <aside className="flex items-center gap-2">
+        <Link href="/agency" className={cn(buttonVariants())}>
+          Get Started
         </Link>
-        <UserButton />
+        <UserButton afterSignOutUrl="/" />
         <ModeToggle />
       </aside>
-    </div>
+    </header>
   )
 }
 
