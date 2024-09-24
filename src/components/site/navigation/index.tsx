832123fb7fd8b2d543544ba3a3da64b8ca3dffc1
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-09-24 12:02:36 +0530

    chore: Update navigation component styles and links
    Refactor the navigation component styles to improve layout and readability. Update the links to use consistent styling and add accessibility attributes.

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -16,15 +16,15 @@ const novaSquare = localFont({
 
 const Navigation = ({ user }: Props) => {
   return (
-    <div className="p-4 flex items-center justify-between z-10 backdrop-blur-lg">
+    <div className="absolute top-0 left-0 right-0 z-10 p-4 flex items-center justify-between backdrop-blur-lg">
       <aside className="flex items-center">
         <span className={`text-xl font-bold ${novaSquare.className}`}> Biznex</span>
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
-          <Link href={'#'}>Pricing</Link>
-          <Link href={'#'}>About</Link>
-          <Link href={'#'}>Features</Link>
+          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>Pricing</Link>
+          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>About</Link>
+          <Link className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-10 text-inherit p-0 underline-offset-8" href={'#'}>Features</Link>
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
