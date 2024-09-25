211f5d089b9d2dbd3c2e81c9fd80ce57d130a0e7
abhirupkumar <abhirupkumar2003@gmail.com>
2024-09-25 20:56:20 +0530

    Refactor UI components and add new features
    - Refactor AspectRatio, Skeleton, Collapsible, Label, Textarea, Separator, Toaster, Progress, Input, Checkbox, and Slider components for improved consistency and readability
    - Add AgencyDetails component for creating an agency
    - Update layout to include Toaster component for displaying notifications

--- src/components/forms/agency-details.tsx
+++ src/components/forms/agency-details.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+"use client";
 
-const AgencyDetails = () => {
+import { useToast } from '@/hooks/use-toast';
+import { Agency } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { AlertDialog } from '@/components/ui/alert-dialog'
+
+type Props = {
+    data?: Partial<Agency>
+}
+
+const AgencyDetails = ({ data }: Props) => {
+    const { toast } = useToast()
+    const router = useRouter()
+    const [deletingAgency, setDeletingAgency] = useState(false)
     return (
-        <div>
-
-        </div>
+        <AlertDialog ></AlertDialog>
     )
 }
 
