83f2ea14a3910c745bb1e4e360607f190c9441b5
abhirupkumar <abhirupkumar2003@gmail.com>
2024-10-23 20:49:47 +0530

    Refactor: Update site editor page component and add site settings and steps components
    - Update the site editor page component to include the new site settings and steps components.
    - Add the site settings component to handle the settings tab in the site editor.
    - Add the site steps component to handle the steps tab in the site editor.
    - Update the import statements to include the new components.
    - Add a link to navigate back to the sites list page.
    - Use tabs to switch between the steps and settings tabs in the site editor.
    - Pass necessary props to the site steps and site settings components.

--- src/app/(main)/project/[projectId]/sites/page.tsx
+++ src/app/(main)/project/[projectId]/sites/page.tsx
@@ -1,7 +1,11 @@
 import { getProjectSite } from '@/lib/queries'
 import React from 'react'
 import SiteForm from '@/components/forms/site-form'
+import SiteSettings from './_components/site-settings'
+import SiteSteps from './_components/site-steps'
 import BlurPage from '@/components/global/blur-page'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import Link from 'next/link'
 
 const Sites = async ({ params }: { params: { projectId: string } }) => {
     const site = await getProjectSite(params.projectId)
@@ -9,9 +13,40 @@ const Sites = async ({ params }: { params: { projectId: string } }) => {
         <SiteForm projectId={params.projectId}></SiteForm>
     </BlurPage>
 
-    return <BlurPage>
-        <div>site</div>
-    </BlurPage>
+
+    return (
+        <BlurPage>
+            <Link
+                href={`/subaccount/${params.projectId}/sites`}
+                className="flex justify-between gap-4 mb-4 text-muted-foreground"
+            >
+                Back
+            </Link>
+            <Tabs
+                defaultValue="steps"
+                className="w-full"
+            >
+                <TabsList className="grid  grid-cols-2 w-[50%] bg-transparent ">
+                    <TabsTrigger value="steps">Steps</TabsTrigger>
+                    <TabsTrigger value="settings">Settings</TabsTrigger>
+                </TabsList>
+                <TabsContent value="steps">
+                    <SiteSteps
+                        site={site}
+                        projectId={params.projectId}
+                        pages={site.SitePages}
+                        siteId={params.projectId}
+                    />
+                </TabsContent>
+                <TabsContent value="settings">
+                    <SiteSettings
+                        projectId={params.projectId}
+                        defaultData={site}
+                    />
+                </TabsContent>
+            </Tabs>
+        </BlurPage>
+    )
 
 }
 
