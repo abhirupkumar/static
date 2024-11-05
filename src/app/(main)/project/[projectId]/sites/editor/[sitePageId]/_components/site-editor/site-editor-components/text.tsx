1ddc7594fde0ea6b6c2caaa3eac4f6a589fd7716
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-11-05 12:14:57 +0530

    Refactor text component to handle special case for "Stories & Insights" content

--- src/app/(main)/project/[projectId]/sites/editor/[sitePageId]/_components/site-editor/site-editor-components/text.tsx
+++ src/app/(main)/project/[projectId]/sites/editor/[sitePageId]/_components/site-editor/site-editor-components/text.tsx
@@ -21,7 +21,7 @@ const TextComponent = (props: Props) => {
     }
     const styles = props.element.styles
     if (!Array.isArray(props.element.content) && props.element.content.innerText == "Stories & Insights") {
-        console.log(styles)
+        console.log(props.element)
     }
 
     const handleOnClickBody = (e: React.MouseEvent) => {
